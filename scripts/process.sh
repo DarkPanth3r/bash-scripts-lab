58571fd45da05f3048f5823ae859d853143da597
mpais <marcospais@outlook.pt>
2022-07-24 00:57:39 +0200

    Update on proccess script

--- scripts/process.sh
+++ scripts/process.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 echo "Hello $USER"
-echo "Hey i am" $USER "and will be telling you about the current processes"
+echo "Hey i am boot and will be telling you about the current processes:"
 echo "Running processes List"
 ps
+echo "Now I will create an file with the sensor data from you PC:"
+sensors > sensor.txt
+echo $PWD"/sensor.txt"
