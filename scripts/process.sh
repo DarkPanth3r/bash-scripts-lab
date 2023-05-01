cf1ba6c9a45d52ed0eab19f63b2d22ca5b0847af
DarkPanth3r <marcospais@outlook.pt>
2023-05-01 21:38:59 +0200

    proccesses updated

--- scripts/process.sh
+++ scripts/process.sh
@@ -2,7 +2,10 @@
 echo "Hello $USER"
 echo "Hey i am boot and will be telling you about the current processes:"
 echo "Running processes List"
-ps
+ps aux
+echo "All proccess where saved on the file:"
+ps aux > ps_aux.txt
+echo $PWD"/ps_aux.txt"
 echo "Now I will create an file with the sensor data from you PC:"
 sensors > sensor.txt
 echo $PWD"/sensor.txt"
